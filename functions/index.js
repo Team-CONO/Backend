7c075653ba607caff11e766f065d011bd9a177bc
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-03-03 22:00:00 +0900

    클라이언트로부터 지정 유저 uid 값 넘겨 받기

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ global.admin = require('firebase-admin');
 // Packages
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')({ origin: true })
+const cors = require('cors')({origin: true})
 
 const authManager = require('./Attribute/AuthManager');
 
@@ -11,12 +11,26 @@ const authApi = express();
 
 const serviceAccount = require('./service-account.json')
 const projectInfo = {
-    databaseURL: functions.config().project.database,
-    storageBucket: functions.config().project.storage,
-    projectId: functions.config().project.project_id
+    databaseURL: functions
+        .config()
+        .project
+        .database,
+    storageBucket: functions
+        .config()
+        .project
+        .storage,
+    projectId: functions
+        .config()
+        .project
+        .project_id
 }
-projectInfo['credential'] = global.admin.credential.cert(serviceAccount)
-global.admin.initializeApp(projectInfo)
+projectInfo['credential'] = global
+    .admin
+    .credential
+    .cert(serviceAccount)
+global
+    .admin
+    .initializeApp(projectInfo)
 
 authApi.use(cors);
 authApi.use(authManager.verifyToken)
@@ -24,25 +38,32 @@ authApi.post('/token', (req, res) => {
     // res.send("ok");/
     var db = admin.database();
     var ref = db.ref(`accounts/${req.currentUID}/role`);
-    ref.once("value", function(snapshot) {
+    ref.once("value", function (snapshot) {
         const role = snapshot.val() || 'Guest';
-        global.admin.auth().createCustomToken(req.currentUID, {role: role})
-        .then(function(customToken) {
-            // Send token back to client
-            res.send({
-                success: true,
-                token: customToken
+        global
+            .admin
+            .auth()
+            .createCustomToken(req.currentUID, {role: role})
+            .then(function (customToken) {
+                // Send token back to client
+                res.send({success: true, token: customToken})
+                return true;
             })
-            return true;
-        })
-        .catch(function(error) {
-            res.send({
-                success: false,
-                message: error.message
-            })
-            return false;
-        });
-    });  
+            .catch(function (error) {
+                res.send({success: false, message: error.message})
+                return false;
+            });
+    });
+})
+authApi.post('/admin', (req, res) => {
+    // res.send('check')
+    let user_uid = req
+        .body
+        console
+        .log(user_uid);
 })
 
-exports.auth = functions.region('asia-northeast1').https.onRequest(authApi)
+exports.auth = functions
+    .region('asia-northeast1')
+    .https
+    .onRequest(authApi)
